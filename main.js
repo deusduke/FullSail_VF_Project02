47bec0f12b135d7b71456d7f261b2b25e312344d
Deus Duke <deus@deusenterprises.com>
2013-03-13 23:10:26 -0500

    began implementing js to store file and validate fields

---
+++ main.js
@@ -0,0 +1,23 @@
+/**
+*	Visual Frameworks Project 2
+*	By: Deus Duke
+*	Term:  1303
+**/
+
+/**
+ * Validations fields, if and error if found and alert
+ * is displdyed
+ * @return {bool} true if valid, otherwise false
+ */
+function validateFields()
+{
+}
+
+/**
+ * Create a project and store it to the client */
+function createProject()
+{
+	// validate all fields, if information is missing or
+	// incorrect return
+	if (!informationValid()) return;
+}
