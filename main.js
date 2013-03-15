dc47862fab51ae6f90969df40061c99c1e4ed9bc
Deus Duke <deus@deusenterprises.com>
2013-03-14 22:53:55 -0500

    - cleaned up and ready for submit

--- main.js
+++ main.js
@@ -10,6 +10,16 @@
  * @return {bool} true if valid, otherwise false
  */
 
+// this string is used to display a project as html
+var projectItemHtml = " \
+<div class='project_list_item'> \n\
+	<h3>{0}</h3> \n\
+	<p>{1}</p> \n\
+	<p>{2}</p> \n\
+	<p>{3}</p> \n\
+</div> \
+";
+
 // utility function to create formatted string similar to .Net
 String.prototype.format = function() {
 	console.log(arguments);
@@ -102,9 +112,36 @@ function retrieveProjects() {
 
 // hide form and show the project list
 function showAllProjects(){
+	// if we are already viewing all projects, return
+	if (document.getElementById('project_list').className != 'hide') return;
+
 	projects = retrieveProjects();
 
-	console.log(projects);
+
+	document.getElementById("mainForm").className = "hide";
+	document.getElementById("showAll").className = 'hide';
+	document.getElementById("project_list").className = '';
+	document.getElementById("addNew").className = '';
+
+	// add the project items to the list
+	var projectListHtml = ""
+	for(var i in projects) {
+		var project = projects[i];
+
+		var projectAsHtml = projectItemHtml.format(
+										project.name, 
+										project.startDate, 
+										project.type, 
+										project.priority);
+		projectListHtml += ('\n' + projectAsHtml);
+
+	}
+
+	console.log(projectListHtml);
+
+
+	// finally we need to insert into the page
+	document.getElementById("project_list").innerHTML = projectListHtml;
 }
 
 // clear all stored data
@@ -112,6 +149,8 @@ function clearAllProjects(){
 	localStorage.clear();	
 
 	alert("All projects have been removed");
+
+	document.getElementById("project_list").innerHTML = '';
 }
 
 // get the value from an array of radiout button
@@ -124,4 +163,12 @@ function getValueFromRadioButtons(arrRadioButtons) {
 
 	// nothing was checked
 	return null;
+}
+
+// add new project
+function addNew() {
+	document.getElementById("mainForm").className = "";
+	document.getElementById("showAll").className = '';
+	document.getElementById("project_list").className = 'hide';
+	document.getElementById("addNew").className = 'hide';
 }
